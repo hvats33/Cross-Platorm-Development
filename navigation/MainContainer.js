c4e397bc787b875b2d9ea505f83a8756b4cd3dce
hvats33 <59967864+hvats33@users.noreply.github.com>
2022-07-03 04:17:12 -0400

    complete app
    navigation

---
+++ navigation/MainContainer.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+import HistoryScreen from './screens/JobHistory';
+import ScheduleScreen from './screens/JobScheduled';
+import ProfileScreen from './screens/Profile';
+
+//Screen names
+const homeName = "Home";
+const historyName = "History";
+const scheduleName = "Schedule";
+const profileName = "Profile";
+
+const Tab = createBottomTabNavigator();
+
+function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? 'home' : 'home-outline';
+
+            } else if (rn === historyName) {
+              iconName = focused ? 'list' : 'list-outline';
+
+            } else if (rn === scheduleName) {
+              iconName = focused ? 'calendar' : 'calendar-outline';
+            }else if (rn === profileName) {
+                iconName = focused ? 'person' : 'person-outline';
+              }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'grey',
+          labelStyle: { paddingBottom: 10, fontSize: 10 },
+          style: { padding: 10, height: 70}
+        }}>
+
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={historyName} component={HistoryScreen} />
+        <Tab.Screen name={scheduleName} component={ScheduleScreen} />
+        <Tab.Screen name={profileName} component={ProfileScreen} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainContainer;
